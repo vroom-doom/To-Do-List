13329b94af48abb3d4d2b042b0cf740a7879265a
vroom-doom <143488552+vroom-doom@users.noreply.github.com>
2024-08-14 02:18:59 +0530

    Updated functionality
    Added basic functionality:
    1) Ability to tick/untick tasks
    2) Ability to add new tasks

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-var i1 = document.getElementById("i1");
-// '<li class="item" id="i1"><input type="checkbox"><s>Eat</s></li><br>';
-console.log(i1.textContent);
+var listLength = 5;
+
+function select(part){  // Function to tick off a task
+    console.log(part);
+    var id = "i" + part.toString();
+    var elem = document.getElementById(id);
+    var text = elem.innerText;
+    console.log(id);
+    elem.classList.toggle("list-done");
+    elem.innerHTML = '<input type="checkbox" checked="true" onclick="onClick(this.id)" id="'+part+'"><s>'+text+'</s><br>';
+    console.log(elem.classList);
+}
+
+function deselect(part){    // Function to untick a task
+    console.log(part);
+    var id = "i" + part.toString();
+    var elem = document.getElementById(id);
+    var text = elem.innerText;
+    console.log(id);
+    elem.classList.remove("list-done");
+    elem.innerHTML = '<input type="checkbox" onclick="onClick(this.id)" id="'+part+'">'+text+'<br>';
+    console.log(elem.classList);
+}
+
+function onClick(clicked_id){   // Function to add basic tick / untick functionality
+    var task = document.getElementById(clicked_id);
+
+    if (task.checked === true){
+        select(clicked_id);
+        console.log(task.checked);
+    }else{
+        console.log("Else called");
+        deselect(clicked_id);
+        console.log(task.checked);
+    }
+}
+
+function addContent(text){
+    listLength += 1;
+    var text = document.getElementById("add").value;
+    if (text != null && text != ""){
+        document.getElementById("list-container").innerHTML+= '<li class="item" id="i'+listLength+'"><input type="checkbox" onclick="onClick(this.id)" id="'+listLength+'">'+text+'</li><br>';
+    }
+    
+}
 
 
 
